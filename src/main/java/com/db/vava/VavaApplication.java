f6a2815907f9d2ef913c04d0972eb04eba92abb2
taddo3 <tadeasdrahovsky@gmail.com>
2020-05-19 20:46:42 +0200

    Globally allowed all origins

--- src/main/java/com/db/vava/VavaApplication.java
+++ src/main/java/com/db/vava/VavaApplication.java
@@ -2,6 +2,12 @@ package com.db.vava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class VavaApplication {
@@ -10,6 +16,19 @@ public class VavaApplication {
 
 		SpringApplication.run(VavaApplication.class, args);
 
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource()
+	{
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+		configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
